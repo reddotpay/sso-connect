47418fd9285a4ad40537001c5bc4f9582118b736
Leroy Lee <leroyleejunhong@gmail.com>
2018-12-28 13:46:48 +0800

    add localstorage.js

---
+++ localstorage.js
@@ -0,0 +1,50 @@
+class localStorageClass {
+
+	constructor() {
+		this.storageAvailable = true;
+		if (typeof (Storage) ==='undefined') {
+			this.storageAvailable = false;
+		}
+	}
+
+	storeLocal(key, value) {
+		if (!this.storageAvailable) {
+			return false;
+		}
+		localStorage.setItem(key, value);
+		return true;
+	}
+
+	getLocal(key) {
+		if (!this.storageAvailable) {
+			return false;
+		}
+		const str = localStorage.getItem(key);
+		if (!str) {
+			return false;
+		}
+		return str;
+	}
+
+	removeLocal(key) {
+		if (!this.storageAvailable) {
+			return false;
+		}
+		localStorage.removeItem(key);
+		return true;
+	}
+
+	clearAllLocal() {
+		if (!this.storageAvailable) {
+			return false;
+		}
+		localStorage.clear();
+		return true;
+	}
+
+}
+
+const LS = localStorageClass;
+const ls = new LS();
+
+export default ls;
