670919f7f78c8cc1c8c81f51986301aded98a6a5
Leroy Lee <leroyleejunhong@gmail.com>
2018-12-28 13:46:17 +0800

    add jwt.js

---
+++ jwt.js
@@ -0,0 +1,34 @@
+import * as jsonwebtoken from 'jsonwebtoken';
+
+class jwtClass {
+	constructor() {
+		this.jwt = jsonwebtoken;
+		this.algo = 'RS256';
+		this.public_key = process.env.VUE_APP_RDP_SSO_PUB;
+		this.issuer = process.env.VUE_APP_RDP_SSO_ISS;
+		this.options = {
+			algorithm: this.algo,
+			issuer: this.issuer,
+		};
+	}
+
+	verifyToken(token) {
+		try {
+			const verify = this.jwt.verify(token, this.public_key, this.options);
+			console.log(verify);
+			return verify;
+		} catch (err) {
+			console.log(err);
+			return false;
+		}
+	}
+}
+
+// module.exports = new jwtClass();
+const JWT = jwtClass;
+const jwt = new JWT();
+// module.exports = jwt;
+
+export default jwt;
+// export jwt;
+
